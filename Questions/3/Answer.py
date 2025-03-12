5ba4794d461a6e873c3137369c3d69f44d2eafa6
Derick Bessa <derickbessa12@gmail.com>
2025-03-12 18:24:44 -0300

    questao_3

--- Questions/3/Answer.py
+++ Questions/3/Answer.py
@@ -13,14 +13,14 @@ R , G , B = cv2.split(img_rgb)
 
 
 
-R_img = cv2.merge([R, np.zeros_like(G), np.zeros_like(B)])
-G_img = cv2.merge([np.zeros_like(R), G, np.zeros_like(B)])
-B_img = cv2.merge([np.zeros_like(R), np.zeros_like(G), B])
+R_img = cv2.merge([np.zeros_like(R), np.zeros_like(G), R])
+G_img = cv2.merge([np.zeros_like(B), G, np.zeros_like(R)])
+B_img = cv2.merge([B, np.zeros_like(G), np.zeros_like(B)])
 
 cv2.imshow ('canal_vermelho.jpg', R_img)
 cv2.imshow ('canal_verde.jpg', G_img)
 cv2.imshow ('canal_azul.jpg', B_img)
-cv2.imshow ('imagem.rgb', img_rgb)
+cv2.imshow ('imagem.jpg', img_rgb)
 
 ######################## agora irei salvar
 
@@ -29,6 +29,9 @@ cv2.imwrite("canalverde.jpg", G_img)
 cv2.imwrite("canal_azul.jpg", B_img)
 cv2.imwrite("imagem_rgb.jpg", img_rgb)
 
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+########### cosiderações: como vc pode observar a imagem em rgb ficou diferente do esperado
+# pois ela foi convertida de BGR, por isso o alguma de suas posicoes de cores foram invertidas
